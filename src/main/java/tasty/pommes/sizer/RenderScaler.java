ecab640368a5143b440966aec6c781b364f8e332
itspronouncedkaesar <unidoublem@gmail.com>
2026-02-09 17:55:44 -0500

    Create RenderScaler.java

---
+++ src/main/java/tasty/pommes/sizer/RenderScaler.java
@@ -0,0 +1,19 @@
+package tasty.pommes.sizer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
+public class RenderScaler {
+
+    @SubscribeEvent
+    public static void onRenderPlayer(RenderPlayerEvent.Pre event) {
+        PoseStack stack = event.getPoseStack();
+        // Corrected: get Double, cast to float for rendering
+        double scale = Config.PLAYER_SCALE.get();
+        stack.scale((float) scale, (float) scale, (float) scale);
+    }
+}
