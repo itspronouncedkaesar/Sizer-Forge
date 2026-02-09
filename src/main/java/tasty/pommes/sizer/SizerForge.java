d3014fce1edfb2f4d7aff8f638c2a44ff826a078
itspronouncedkaesar <unidoublem@gmail.com>
2026-02-09 18:57:00 -0500

    Create SizerForge.java

---
+++ src/main/java/tasty/pommes/sizer/SizerForge.java
@@ -0,0 +1,27 @@
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.IEventBusSubscriber;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+@Mod(SizerForge.MOD_ID)
+public class SizerForge {
+    public static final String MOD_ID = "sizer";
+
+    public SizerForge() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        // Register config and setup events
+        modBus.addListener(this::commonSetup);
+
+        // Register render scaler for client
+        MinecraftForge.EVENT_BUS.register(RenderScaler.class);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Register the mod config in the appropriate way
+        ModConfig.register(Config.SPEC);
+    }
+}
