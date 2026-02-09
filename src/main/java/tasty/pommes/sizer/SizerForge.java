32a5647eee22a5ceb6f4b86e94ce9b4e1b498417
itspronouncedkaesar <unidoublem@gmail.com>
2026-02-09 17:29:05 -0500

    Add files via upload

---
+++ src/main/java/tasty/pommes/sizer/SizerForge.java
@@ -0,0 +1,16 @@
+package tasty.pommes.sizer;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(SizerForge.MOD_ID)
+public class SizerForge {
+    public static final String MOD_ID = "sizer";
+
+    public SizerForge() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(SizeHandler.class);
+    }
+}
